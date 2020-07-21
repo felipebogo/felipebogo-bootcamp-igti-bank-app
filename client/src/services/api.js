c160005c758f9180631743d2ed6afd84fb36cd13
Felipe Bogo <felipe.bogo@jupiter.co.ao>
2020-07-21 04:12:25 +0100

    proxy back

--- client/src/services/api.js
+++ client/src/services/api.js
@@ -1,7 +1,9 @@
 import axios from 'axios';
 
 const api = axios.create({
-  baseURL: 'http://localhost:3001',
+  baseURL: `http://localhost:${process.env.PORT}`,
 });
+
+console.log('url: ',`http://localhost:${process.env.PORT}`);
 
 export default api;
